7d3ba625985a182997a65b72c3cf3c4db3cf6895
administrator <despino@g.ucla.edu>
2024-01-25 15:01:23 -0800

    CNN for Voltage Image Segmentation

---
+++ Train.py
@@ -0,0 +1,129 @@
+import torch
+from torch.utils.data import DataLoader, Dataset
+from torch.utils.tensorboard import SummaryWriter
+import os
+import tifffile as tif
+import numpy as np
+from datetime import datetime
+from Model import UNet
+
+class NeuronSegmentData(Dataset):
+
+    """Voltage Image Neuron Segtmentation Dataset"""
+
+    def __init__(self, root_dir):
+        """
+        Arguments:
+            root_dir (string): Directory with all the images.
+        """
+        self.root_dir = root_dir
+        self.files = os.listdir(root_dir)
+
+    def __len__(self):
+        return len(os.listdir(self.root_dir))
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        img_name = os.path.join(self.root_dir,self.files[idx])
+        imageData = tif.imread(img_name)
+
+        img = np.array(imageData[0:2])
+        img = torch.Tensor(img)
+
+        lbl = np.array([imageData[-1]])
+        lbl = torch.Tensor(lbl)
+
+        return img, lbl
+    
+def train_epoch(idx, tb_writer, training_loader, model, loss_fn, optimizer):
+    running_loss = 0.
+    last_loss = 0.
+
+    # Here, we use enumerate(training_loader) instead of
+    # iter(training_loader) so that we can track the batch
+    # index and do some intra-epoch reporting
+    for i, data in enumerate(training_loader):
+        # Every data instance is an input + label pair
+        inputs, labels = data
+
+        # Zero your gradients for every batch!
+        optimizer.zero_grad()
+
+        # Make predictions for this batch
+        outputs = model(inputs)
+
+        # Compute the loss and its gradients
+        loss = loss_fn(outputs, labels)
+        loss.backward()
+
+        # Adjust learning weights
+        optimizer.step()
+
+        # Gather data and report
+        running_loss += loss.item()
+        if i % 1000 == 999:
+            last_loss = running_loss / 1000 # loss per batch
+            print('  batch {} loss: {}'.format(i + 1, last_loss))
+            tb_x = idx * len(training_loader) + i + 1
+            tb_writer.add_scalar('Loss/train', last_loss, tb_x)
+            running_loss = 0.
+
+    return last_loss
+
+def train_network():
+    model = UNet()
+    training_loader = DataLoader(NeuronSegmentData(root_dir='TrainingData'), batch_size=4, shuffle=True, num_workers=4)
+    validation_loader = DataLoader(NeuronSegmentData(root_dir='ValidationData'), batch_size=4, shuffle=True, num_workers=4)
+    loss_fn = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.RMSprop(model.parameters(), lr=0.001, momentum=0.9)
+    timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+    writer = SummaryWriter('runs/VoltImSeg_Trainer_{}'.format(timestamp))
+    epoch_number = 0
+
+    EPOCHS = 5
+
+    best_vloss = 1_000_000.
+
+    for epoch in range(EPOCHS):
+        print('EPOCH {}:'.format(epoch_number + 1))
+
+        # Make sure gradient tracking is on, and do a pass over the data
+        model.train(True)
+        avg_loss = train_epoch(epoch_number, writer, training_loader, model, loss_fn, optimizer)
+
+
+        running_vloss = 0.0
+        # Set the model to evaluation mode, disabling dropout and using population
+        # statistics for batch normalization.
+        model.eval()
+
+        # Disable gradient computation and reduce memory consumption.
+        with torch.no_grad():
+            for i, vdata in enumerate(validation_loader):
+                vinputs, vlabels = vdata
+                voutputs = model(vinputs)
+                vloss = loss_fn(voutputs, vlabels)
+                running_vloss += vloss
+
+        avg_vloss = running_vloss / (i + 1)
+        print('LOSS train {} valid {}'.format(avg_loss, avg_vloss))
+
+        # Log the running loss averaged per batch
+        # for both training and validation
+        writer.add_scalars('Training vs. Validation Loss',
+                        { 'Training' : avg_loss, 'Validation' : avg_vloss },
+                        epoch_number + 1)
+        writer.flush()
+
+        # Track best performance, and save the model's state
+        if avg_vloss < best_vloss:
+            best_vloss = avg_vloss
+            model_path = 'models/model_{}_{}'.format(timestamp, epoch_number)
+            torch.save(model.state_dict(), model_path)
+
+        epoch_number += 1
+
+if __name__ == '__main__':
+    train_network()
